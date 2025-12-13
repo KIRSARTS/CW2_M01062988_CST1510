e8da2ba5544b6b9fb9a54dd6730ca00c2636c795
Kirsten Torgbor <Kirstenwillington678@gmail.com>
2025-12-13 09:53:48 +0000

    Modified file

--- app/users.py
+++ app/users.py
@@ -4,16 +4,16 @@
 
 
 
-def add_user(conn, name, password):
+def add_user(conn, name, hash):
     curr = conn.cursor()
     sql = """ INSERT INTO users (username, password_hash) VALUES (?, ?) """
-    param = (name, password)
+    param = (name, hash)
     curr.execute(sql,param)
     conn.commit()
     
     
     
-  def migrate_users(conn):
+def migrate_users(conn):
     with open('DATA/users.txt')as f:
         users = f.readlines()
         
