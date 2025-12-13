38765b2dd0219b1ec528ffdf7bf52bf1ee28138a
Kirsten Torgbor <Kirstenwillington678@gmail.com>
2025-12-13 20:43:08 +0000

    Modified file

--- app/tickets.py
+++ app/tickets.py
@@ -4,3 +4,10 @@ import pandas as pd
 def migrate_it_tickets(conn):
     data_metadata = pd.read_csv('DATA/it_tickets.csv') # 1 read data from csv
     data_metadata.to_sql('it_tickets', conn) # 3. create table, inserting the data
+    
+    
+def get_all_it_tickets(conn):
+    sql = 'SELECT * FROM it_tickets'
+    data = pd.read_sql(sql, conn)
+    conn.close()
+    return data        
