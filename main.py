9be11cbe0ccb93721fd28fa9b40ec7481e795ac6
Kirsten Torgbor <Kirstenwillington678@gmail.com>
2025-12-17 18:25:37 +0000

    Register hash and login files(updated)

--- main.py
+++ main.py
@@ -13,20 +13,25 @@ def menu():
     
 def main():
     while True:
-        menu()
-        choice = input('>')
+        print('*** Welcome to my system ***')  
+    print('Choose from the following options:')
+    print('1. Register') 
+    print('2. Log in') 
+    print('3. Exit!')
+    
+    choice = input('>')
         
-        if choice == '1':
+    if choice == '1':
             register_user()
             print('User registered!!')
-        elif choice == '2':
+    elif choice == '2':
             if log_in_user():
                 print('You are logged in !!!!!')
             else:
                 print('Incorrect log in')
-        elif choice == '3':
+    elif choice == '3':
             print('Good bye!!')
-            break                 
+    break                 
     
     
       
