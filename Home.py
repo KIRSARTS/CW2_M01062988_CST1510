bf0450b686ffc9d3b8e3f9e1073594bea736074b
Kirsten Torgbor <Kirstenwillington678@gmail.com>
2025-12-19 10:16:49 +0000

    Login and registration file

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from appl import hash_password, validate_hash
-from app.users import add_user
+from app.users import add_user, get_user
 from app.db import get_db_connection
 
 conn = get_db_connection()
@@ -16,10 +16,19 @@ if'logged_in' not in st.session_state:
 tab_login, tab_register = st.tabs(["Login", "Register"])
 
 with tab_login:
+    login_username = st.text_input("Username")
+    login_password = st.text_input("Password", type = "password")
+    
     if st.button("Log In"):
-        st.session_state['logged_in'] = True
-    st.success("You are now logged in.")
-
+        id, name, password = get_user(conn, login_username)
+        if not validate_hash(login_password, password):
+            st.error("Invalid username or password. Please try again.")
+        else:
+            st.session_state['logged_in'] = True
+            st.success("You are now logged in.")
+          
+   
+# password registration
 with tab_register:
     register_username = st.text_input("New Username")
     register_password = st.text_input("New password", type = "password")
